8baea80e4a46c588b32d8bf45c1bcf1d627d4bcc
elyfalcon <elyfalcon@yahoo.com>
2019-05-23 19:50:37 -0300

    clase 5

---
+++ Clase5Lab3/Ejercicio6/script.js
@@ -0,0 +1,20 @@
+
+var boton;
+var ids= 1;
+
+addEventListener('load', function(){
+
+    boton= document.getElementById('btnRun');
+
+    boton.addEventListener('click', ejecutar, false);
+}, false);
+
+
+
+function ejecutar(){
+
+    //armar una tabla con las personas
+
+   
+    
+}
